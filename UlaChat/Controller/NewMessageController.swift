e932039a36dbb7218a3eef53586454a79a157e0b
Ula <uladzislau.daratsiuk@gmail.com>
2018-03-06 11:00:58 +0100

    Working on the cache problem with profile images

--- UlaChat/Controller/NewMessageController.swift
+++ UlaChat/Controller/NewMessageController.swift
@@ -33,6 +33,7 @@ class NewMessageController: UITableViewController {
                 let user = User()
                 user.name = dictionary["name"] as? String
                 user.email = dictionary["email"] as? String
+                user.profileImageUrl = dictionary["profileImageUrl"] as? String
                 self.users.append(user)
                 
                 DispatchQueue.main.async {
@@ -54,21 +55,52 @@ class NewMessageController: UITableViewController {
  
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
         
-     //   let cell = UITableViewCell(style: .subtitle, reuseIdentifier: cellId)
-        
-        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath)
+        let cell = tableView.dequeueReusableCell(withIdentifier: cellId, for: indexPath) as? UserCell
         let user = users[indexPath.row]
-        cell.textLabel?.text = user.name
-        cell.detailTextLabel?.text = user.email
+        cell?.textLabel?.text = user.name
+        cell?.detailTextLabel?.text = user.email
         
-        return cell
+        if let profileImageUrl = user.profileImageUrl {
+           cell?.profileImageView.loadimagesUisingCacheWithUrlString(urlString: profileImageUrl)
+        }
+        return cell!
     }
+    
+    override func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        return 56
+    }
+    
 }
 
 class UserCell: UITableViewCell {
     
+    override func layoutSubviews() {
+        super.layoutSubviews()
+        
+        textLabel?.frame = CGRect(origin: CGPoint(x: 56, y: (textLabel?.frame.origin.y)!),size: CGSize(width:(textLabel?.frame.width)!,height: (textLabel?.frame.height)!))
+        detailTextLabel?.frame = CGRect(origin: CGPoint(x: 56, y: (detailTextLabel?.frame.origin.y)!),size: CGSize(width:(detailTextLabel?.frame.width)!,height: (detailTextLabel?.frame.height)!))
+    }
+    
+    let profileImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "photo")
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.layer.cornerRadius = 20
+        imageView.layer.masksToBounds = true
+        return imageView
+    }()
+    
     override init(style: UITableViewCellStyle, reuseIdentifier: String?) {
         super.init(style: .subtitle, reuseIdentifier: reuseIdentifier)
+        
+        addSubview(profileImageView)
+        
+        profileImageView.leftAnchor.constraint(equalTo: self.leftAnchor, constant: 8).isActive = true
+        profileImageView.centerYAnchor.constraint(equalTo: self.centerYAnchor).isActive = true
+        profileImageView.widthAnchor.constraint(equalToConstant: 40).isActive = true
+        profileImageView.heightAnchor.constraint(equalToConstant: 40).isActive = true
+        
+        
     }
     
     required init?(coder aDecoder: NSCoder) {
